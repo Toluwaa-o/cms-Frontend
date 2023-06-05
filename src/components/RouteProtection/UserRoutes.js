f78a469eeb2684f8c2d0ac4b6f28dd4b867fbe59
Toluwalashe <asheogunleye02@gmail.com>
2023-06-05 12:31:34 +0000

    bug fix

--- src/components/RouteProtection/UserRoutes.js
+++ src/components/RouteProtection/UserRoutes.js
@@ -1,27 +1,31 @@
 import { useNavigate } from "react-router";
 import { useEffect } from "react";
 import instance from "../Axios/Config";
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { UserActions } from "../../stores/UserSlice";
 
 export default function UserRoutes(props) {
   const navigate = useNavigate();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const user = useSelector(state => state.user.user)
+  const user = useSelector((state) => state.user.user);
 
   useEffect(() => {
-    instance({
-      url: "/users/show-current-user",
-      method: "get",
-    })
-      .then((res) => {
-        dispatch(UserActions.getUser(res.data.user))
-        if (!res.data.user) return navigate("/login");
+    if (user) {
+      return;
+    } else {
+      instance({
+        url: "/users/show-current-user",
+        method: "get",
       })
-      .catch(() => {
-        return navigate("/login");
-      });
+        .then((res) => {
+          dispatch(UserActions.getUser(res.data.user));
+          if (!res.data.user) return navigate("/login");
+        })
+        .catch(() => {
+          return navigate("/login");
+        });
+    }
   }, []);
 
   return <>{user && props.children}</>;
